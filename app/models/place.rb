4f3b83fb3ed6ced3072c382bdd00bafc212d38a8
Chris Aitchison <cmaitchison@gmail.com>
2011-04-11 21:37:17 +1000

    Refactored Json helper methods into a module

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ActiveRecord::Base
+  include Jsonnable
   
   before_save :refresh_ancestry
   
@@ -48,16 +49,6 @@ class Place < ActiveRecord::Base
     add_field hash, :west
     hash = { self.class.model_name.element => hash } if include_root_in_json
     hash
-  end
-  
-  def add_field hash, attribute_name
-    attribute_value = get_potentially_missing_attribute attribute_name   
-    return if attribute_value.nil? || attribute_value == 'null'
-    hash.merge!({attribute_name.to_s => attribute_value})
-  end
-  
-  def get_potentially_missing_attribute method_name   
-    method(method_name).call if has_attribute? method_name
   end
   
 end
--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,4 +1,5 @@
 class Poi < ActiveRecord::Base
+  include Jsonnable
   belongs_to :place
   before_save :refresh_ancestry
   
@@ -42,14 +43,4 @@ class Poi < ActiveRecord::Base
     hash
   end
 
-  def add_field hash, attribute_name
-    attribute_value = get_potentially_missing_attribute attribute_name   
-    return if attribute_value.nil? || attribute_value == 'null'
-    hash.merge!({attribute_name.to_s => attribute_value})
-  end
-
-  def get_potentially_missing_attribute method_name   
-      method(method_name).call if has_attribute? method_name
-  end
- 
 end
